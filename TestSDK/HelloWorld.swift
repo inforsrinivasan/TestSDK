6407d83b746ceb4a35b31a645fda4d14b767070a
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2021-09-12 15:46:25 +0200

    commit Test SDK

---
+++ TestSDK/HelloWorld.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWorld.swift
+//  TestSDK
+//
+//  Created by Srinivasan Rajendran on 2021-09-12.
+//
+
+public class HelloWorld {
+    let hello = "Hello"
+
+    public init() {}
+
+    public func hello(to whom: String) -> String {
+        return "Hello \(whom)"
+    }
+}
---
+++ TestSDK/TestSDK.h
@@ -0,0 +1,18 @@
+//
+//  TestSDK.h
+//  TestSDK
+//
+//  Created by Srinivasan Rajendran on 2021-09-12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestSDK.
+FOUNDATION_EXPORT double TestSDKVersionNumber;
+
+//! Project version string for TestSDK.
+FOUNDATION_EXPORT const unsigned char TestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSDK/PublicHeader.h>
+
+
